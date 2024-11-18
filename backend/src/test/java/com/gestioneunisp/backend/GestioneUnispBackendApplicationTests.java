da528692c5618112f86d6678d21610a552033956
kmgbrc <kmgbrc@unife.it>
2024-11-18 19:25:44 +0100

    Initial commit with Docker setup

---
+++ backend/src/test/java/com/gestioneunisp/backend/GestioneUnispBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestioneunisp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneUnispBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
